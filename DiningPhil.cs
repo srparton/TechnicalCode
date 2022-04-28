9ece7dc411adf2d98a911b43fda2dc08e1067627
nick-g99 <103017771+nick-g99@users.noreply.github.com>
2022-04-27 22:31:04 -0500

    Merged the "Chop" class as an inner class
    Also made some syntax updates

--- DiningPhil.cs
+++ DiningPhil.cs
@@ -1,42 +1,126 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-public class DiningPhil {
-// public class DiningPhil {
-    
+
+
+public class DiningPhil
+{
+    internal class Chop
+    {
+        private int N;
+        private bool[] available;
+
+        public Chop(int num)
+        {
+            N = num;
+            this.available = new bool[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                this.available[i] = true; // non allocated stick }
+            }
+        }
+
+        public void get_LR(int me)
+        {
+            lock (this)
+            {
+                while (!available[me])
+                {
+                    try
+                    {
+                        Monitor.Wait(this);
+                    }
+                    catch (ThreadInterruptedException e) 
+                    {
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+
+                available[me] = false;
+                Console.WriteLine("Diner " + me + " has left fork " + me);
+                get_R((me + 1) % N);
+            }
+        }
+
+        public void get_R(int me)
+        {
+            while (!available[me])
+            {
+                try
+                {
+                    Monitor.Wait(this);
+                }
+                catch (ThreadInterruptedException e) 
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+            available[me] = false;
+            if (me == 0)
+            {
+                Console.WriteLine("Diner " + me + " has right fork " + me);
+            }
+            else
+                Console.WriteLine("Diner " + (me - 1) + " has right fork " + me);
+        }
+
+        public void release(int me)
+        {
+            available[me] = true;
+            available[(me + 1) % N] = true;
+            Console.WriteLine("Diner " + me + " set forks down");
+            Monitor.Pulse(this);
+        }
+
+        // public void run() {
+        //     try {
+        //         get_LR(thread_id);
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+    }
+    // public class DiningPhil {
+
     public int thread_id; // Used in constructor for DiningPhil
-    private static sealed int EAT = 2; //How many times should each philo eat
-    private static sealed int DINERS_NUM = 5; //Number of philos
+    private static int EAT = 2; //How many times should each philo eat
+    private static int DINERS_NUM = 5; //Number of philos
     private static Chop myChop = new Chop(DINERS_NUM);
 
-    public DiningPhil(int id) {
-        this.thread_id = id;
-    }
+    public DiningPhil(int id) => this.thread_id = id;
 
-    public void run(){
+    public void run()
+    {
         Console.WriteLine("Diner " + thread_id + " is sitting down at the table.");
         int timesEaten = 0;
 
-        while (timesEaten < EAT){
-            int sleepTime = ThreadLocalRandom.current().nextInt(0,2000); //determine a random amount of time between 0 and 2 seconds
+        while (timesEaten < EAT)
+        {
+            Random r = new Random();
+            int sleepTime = r.Next(10000);
             Console.WriteLine("Diner " + thread_id + " says they are thinking.");
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            try
+            {
+                Thread.Sleep(sleepTime);
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.StackTrace);
             } //Diner "Thinks"
             Console.WriteLine("Diner " + thread_id + " says they are hungry.");
-
-
             myChop.get_LR(thread_id);
             timesEaten++;
-
-            sleepTime = ThreadLocalRandom.current().nextInt(0,2000);
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            sleepTime = r.Next(10000);
+            try
+            {
+                Thread.Sleep(sleepTime);
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.StackTrace);
             } //Diner "eats" for rand time between 0 and 2 seconds
 
             myChop.release(thread_id);
@@ -44,22 +128,17 @@ public class DiningPhil {
         Console.WriteLine("Diner " + thread_id + " is full, they are leaving the table.");
     }
 
-    public static void main(String[] args) {
-
+    public static void Main(String[] args)
+    {
         DiningPhil[] philo = new DiningPhil[DINERS_NUM];
+        Task[] dine = new Task[DINERS_NUM];
 
-        // Initiallizing diners array with threads
-        for (int i = 0; i < DINERS_NUM; i++) {
+        for (int i = 0; i < DINERS_NUM; i++)
+        {
             philo[i] = new DiningPhil(i);
-            philo[i].start();
+            dine[i] = Task.Run(philo[i].run);
         }
 
-        for (int i = 0; i < philo.length; i++) {
-            try {
-                philo[i].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        Task.WaitAll(dine);
     }
 }
